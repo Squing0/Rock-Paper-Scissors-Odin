eca1f8cb5f98d501fc204da7a00fe5ea3771604d
Lyle Patterson <lyle.patterson2002@gmail.com>
2024-05-13 14:45:14 +0100

    Finished main logic

--- application.js
+++ application.js
@@ -1,83 +1,116 @@
-let humanScore = 0;
+let playerScore = 0;
 let computerScore = 0;
 let drawScore = 0;
+
+const playerScoreDoc = document.querySelector("#playerNum");
+const comScoreDoc = document.querySelector("#comNum");
+const drawScoreDoc = document.querySelector("#drawNum");
+
+const resetBtn = document.querySelector("#resetBtn");
+
+const resultsText = document.querySelector("#resultsText");
+const roundText = document.querySelector("#roundResultText");
+const playerWinsText = document.querySelector("#playerWins");
+const computerWinsText = document.querySelector("#computerWins");
+
+const buttons = document.querySelectorAll("button");
+const scores = document.querySelectorAll(".scoreNum");
+
 
 function getComputerChoice(){
     let comChoice = Math.floor(Math.random() * 3) + 1;
 
-    return decideChoice(comChoice);
-}
-
-function getHumanChoice(){
-    let userChoice = prompt("Enter rock, paper or scissors.")
-
-    if(userChoice === null){
-        return "Nothing";
-    }
-    userChoice = userChoice.toLowerCase();
-
-    return decideChoice(userChoice);
-}
-
-function decideChoice(choice){
-    switch(choice){
+    switch(comChoice){
         case 1:
             return "Rock";
         case 2:
             return "Scissors";
         case 3:
             return "Paper";
-        case "rock":
-            return "Rock"
-        case "paper":
-            return "Paper";
-        case "scissors":
-            return "Scissors";
-        default:
-            return "Nothing";      
     }
 }
 
-function playRound(userChoice, comChoice){
-    if(userChoice == "Nothing"){
-        return "Incorrect Input";
+function checkScore(){
+    if(computerScore == 5){
+        resultsText.textContent = "Computer Wins!"
+        incrementScore(computerWinsText);
+        endGame();
     }
 
-    if(userChoice == comChoice){
+    if(playerScore == 5){
+        resultsText.textContent = "Player wins!";
+        incrementScore(playerWinsText);
+        endGame();
+    }
+}
+
+function endGame(){
+    buttons.forEach((button) => {
+        button.disabled = true;
+    });
+
+    resultsText.style.visibility = "visible";
+
+    resetBtn.style.visibility = "visible";
+    resetBtn.disabled = false;
+}
+
+function playRound(playerChoice, comChoice){
+    if(playerChoice == comChoice){
         drawScore++;
-        return "Draw!";    
+        incrementScore(drawScoreDoc);
+        roundText.textContent = "Round result: Draw!";    
     }
-
-    if(userChoice == "Rock" && comChoice == "Scissors" || 
-    userChoice == "Scissors" && comChoice == "Paper" || 
-    userChoice == "Paper" && comChoice == "Rock"){
-        humanScore++;
-        return `You Win! ${userChoice} beats ${comChoice}`;    
+    else if(playerChoice == "Rock" && comChoice == "Scissors" || 
+    playerChoice == "Scissors" && comChoice == "Paper" || 
+    playerChoice == "Paper" && comChoice == "Rock"){
+        playerScore++;
+        incrementScore(playerScoreDoc);
+        checkScore();
+        roundText.textContent = `Round result: You Win! ${playerChoice} beats ${comChoice}`;    
     }
     else{
         computerScore++;
-        return `You lose! ${comChoice} beats ${userChoice}`
+        incrementScore(comScoreDoc);
+        checkScore();
+        roundText.textContent = `Round result: You lose! ${comChoice} beats ${playerChoice}`
     }
 }
 
-function playGame(){
-    for(let i = 0; i < 5; i++){
-        console.log(playRound(getHumanChoice(), getComputerChoice()));
-    }
-
-    if(humanScore > computerScore){
-        console.log(`You Win. You won ${humanScore} rounds.
-        The computer won ${computerScore} rounds.
-        There were ${drawScore} draws.`)
-    }
-    else if (humanScore < computerScore){
-        console.log(`You lose. You won ${humanScore} rounds. 
-        The computer won ${computerScore} rounds.
-        There were ${drawScore} draws.`)
-    }
-    else{
-        console.log("Each round was a draw!")
-    }
+function incrementScore(scoreText){
+    scoreText.textContent = parseInt(scoreText.textContent) + 1;
 }
 
-playGame();
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        switch(button.id){
+            case "1":
+                console.log(playRound("Rock", getComputerChoice()));
+                break;
+            case "2":
+                console.log(playRound("Paper", getComputerChoice()));
+                break; 
+            case "3":
+                console.log(playRound("Scissors", getComputerChoice()));
+                break; 
+        };
+    });
+});
+
+resetBtn.addEventListener("click", () => {
+    scores.forEach((score) => {
+        score.textContent = "0";
+    });
+
+    buttons.forEach((button) => {
+        button.disabled = false;
+    });
+
+    playerScore = 0;
+    computerScore = 0;
+    drawScore = 0;
+
+    resetBtn.style.visibility = "hidden";
+    resultsText.style.visibility = "hidden";
+    roundText.textContent = "Round result:"
+});
